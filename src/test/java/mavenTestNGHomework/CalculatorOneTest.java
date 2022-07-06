1dd5901953af4500d678deae6cf8f37928ac660c
sBullis <104388303+sBullis@users.noreply.github.com>
2022-07-06 18:29:59 +0300

    md 9 upd

--- src/test/java/mavenTestNGHomework/CalculatorOneTest.java
+++ src/test/java/mavenTestNGHomework/CalculatorOneTest.java
@@ -6,49 +6,48 @@ import org.testng.annotations.*;
 public class CalculatorOneTest {
     public CalculatorOne calculator;
 
+    @BeforeClass
+    public void beforeClass(){
+        this.calculator = new CalculatorOne();
+    }
+    @BeforeTest
+    public void beforeTest(){
+        System.out.println(" Testēsim kalkulatoru... ");
+    }
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println(" Tests ir sācies! ");
+    }
+
     @Test
     public void testAdd() {
         Assert.assertEquals(calculator.add(1,1),2);
-        Assert.assertEquals(calculator.add(2,2),4);
         Assert.assertEquals(calculator.add(2,-2),0);
+        Assert.assertEquals(calculator.add(-2,-2),-4);
     }
 
     @Test
     public void testSubstract() {
         Assert.assertEquals(calculator.substract(3,3),0);
         Assert.assertEquals(calculator.substract(3,-3),6);
-        Assert.assertEquals(calculator.substract(0,3),-3);
+        Assert.assertEquals(calculator.substract(-3,-3),0);
     }
 
     @Test
     public void testMultiply() {
         Assert.assertEquals(calculator.multiply(2,2),4);
         Assert.assertEquals(calculator.multiply(2,0),0);
-        Assert.assertEquals(calculator.multiply(2,1),2);
+        Assert.assertEquals(calculator.multiply(2,-1),-2);
     }
 
     @Test
     public void testDivide() {
         Assert.assertEquals(calculator.divide(2,2),1);
         Assert.assertEquals(calculator.divide(2,1),2);
-        Assert.assertEquals(calculator.divide(2,-2),-1);
+        Assert.assertEquals(calculator.divide(-2,-2),1);
+//        Assert.assertEquals(calculator.divide(2,0),0);
     }
 
-
-
-    @BeforeTest
-    public void beforeTest(){
-        System.out.println(" Testēsim kalkulatoru... ");
-    }
-
-    @BeforeMethod
-    public void beforeMethod(){
-        System.out.println(" Tests ir sācies! ");
-    }
-    @BeforeClass
-    public void beforeClass(){
-        this.calculator = new CalculatorOne();
-    }
     @AfterMethod
     public void afterMethod(){
         System.out.println(" Viens tests ir beidzies!");
